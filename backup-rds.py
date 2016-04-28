9ca257abaf44ae1aeac3da8e1151d9d652215173
Paulina Budzon <paulina.budzon@gmail.com>
2016-04-28 23:08:51 +0100

    Added functions for copying release AMIs and copying RDS snapshots to frankfurt

---
+++ backup-rds.py
@@ -0,0 +1,102 @@
+import boto3
+import operator
+
+
+def copy_latest_snapshot():
+    client = boto3.client('rds', 'eu-west-1')
+    frankfurt_client = boto3.client('rds', 'eu-central-1')
+
+    response = client.describe_db_snapshots(
+        SnapshotType='automated',
+        IncludeShared=False,
+        IncludePublic=False
+    )
+
+    if len(response['DBSnapshots']) == 0:
+        raise Exception("No automated snapshots found")
+
+    snapshots_per_project = {}
+    for snapshot in response['DBSnapshots']:
+        if snapshot['Status'] != 'available':
+            continue
+
+        if snapshot['DBInstanceIdentifier'] not in snapshots_per_project.keys():
+            snapshots_per_project[snapshot['DBInstanceIdentifier']] = {}
+
+        snapshots_per_project[snapshot['DBInstanceIdentifier']][snapshot['DBSnapshotIdentifier']] = snapshot[
+            'SnapshotCreateTime']
+
+    for project in snapshots_per_project:
+        sorted_list = sorted(snapshots_per_project[project].items(), key=operator.itemgetter(1), reverse=True)
+
+        copy_name = project + "-" + sorted_list[0][1].strftime("%Y-%m-%d")
+
+        print("Checking if " + copy_name + " is copied")
+
+        try:
+            frankfurt_client.describe_db_snapshots(
+                DBSnapshotIdentifier=copy_name
+            )
+        except:
+            response = frankfurt_client.copy_db_snapshot(
+                SourceDBSnapshotIdentifier='arn:aws:rds:eu-west-1:800540593336:snapshot:' + sorted_list[0][0],
+                TargetDBSnapshotIdentifier=copy_name,
+                CopyTags=True
+            )
+
+            if response['DBSnapshot']['Status'] != "pending" and response['DBSnapshot']['Status'] != "available":
+                raise Exception("Copy operation for " + copy_name + " failed!")
+            print("Copied " + copy_name)
+
+            # frankfurt_client.modify_db_snapshot_attribute(
+            #     DBSnapshotIdentifier=copy_name,
+            #     AttributeName='restore',
+            #     ValuesToAdd=['678957977264']
+            # )
+
+            continue
+
+        print("Already copied")
+
+
+def remove_old_snapshots():
+    client = boto3.client('rds', 'eu-west-1')
+    frankfurt_client = boto3.client('rds', 'eu-central-1')
+
+    response = frankfurt_client.describe_db_snapshots(
+        SnapshotType='manual'
+    )
+
+    if len(response['DBSnapshots']) == 0:
+        raise Exception("No manual snapshots in Frankfurt found")
+
+    snapshots_per_project = {}
+    for snapshot in response['DBSnapshots']:
+        if snapshot['Status'] != 'available':
+            continue
+
+        if snapshot['DBInstanceIdentifier'] not in snapshots_per_project.keys():
+            snapshots_per_project[snapshot['DBInstanceIdentifier']] = {}
+
+        snapshots_per_project[snapshot['DBInstanceIdentifier']][snapshot['DBSnapshotIdentifier']] = snapshot[
+            'SnapshotCreateTime']
+
+    for project in snapshots_per_project:
+        if len(snapshots_per_project[project]) > 1:
+            sorted_list = sorted(snapshots_per_project[project].items(), key=operator.itemgetter(1), reverse=False)
+            to_remove = [i[0] for i in sorted_list[1:]]
+
+            for snapshot in to_remove:
+                print("Removing " + snapshot)
+                frankfurt_client.delete_db_snapshot(
+                    DBSnapshotIdentifier=snapshot
+                )
+
+
+def lambda_handler(event, context):
+    copy_latest_snapshot()
+    remove_old_snapshots()
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
--- clean-release-images.py
+++ clean-release-images.py
@@ -1,14 +1,14 @@
+from __future__ import print_function
 import boto3
 import operator
 
 
-def lambda_handler(event, context):
-    LIMIT = 10
-    client = boto3.client('ec2', 'eu-west-1')
+def clean_images(region, limit):
+    client = boto3.client('ec2', region)
 
     response = client.describe_images(
         Owners=['self'],
-        Filters=[{'Name': 'tag:Type', 'Values': ['BaseImage']}]
+        Filters=[{'Name': 'tag:Type', 'Values': ['ReleaseImage']}]
     )
 
     if len(response['Images']) == 0:
@@ -23,11 +23,11 @@ def lambda_handler(event, context):
                 images[tag['Value']][image['ImageId']] = image['CreationDate']
                 break
 
-    to_remove = []
+    to_remove = [];
     for project in images:
         sorted_x = sorted(images[project].items(), key=operator.itemgetter(1), reverse=True)
-        if len(sorted_x) > LIMIT:
-            to_remove = to_remove + [i[0] for i in sorted_x[LIMIT:]]
+        if len(sorted_x) > limit:
+            to_remove = to_remove + [i[0] for i in sorted_x[limit:]]
 
     if len(to_remove) == 0:
         print("Nothing to do")
@@ -38,6 +38,11 @@ def lambda_handler(event, context):
     for ami in to_remove:
         print("Removing: " + ami)
         client.deregister_image(ImageId=ami)
+
+
+def lambda_handler(event, context):
+    clean_images('eu-west-1', 50)
+    clean_images('eu-central-1', 1)
 
 
 if __name__ == '__main__':
